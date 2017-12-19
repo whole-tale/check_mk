0b2baaafecaf52ed72671fe807a1ab5f46a35a9f
Craig Willis <willis8@illinois.edu>
2017-12-19 13:19:34 -0600

    Added directory creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mkdir -p /usr/lib/check_mk_agent/local
 cp /scripts/check-services /usr/lib/check_mk_agent/local
 cp /scripts/check-nodes /usr/lib/check_mk_agent/local
 cp /scripts/check-celery /usr/lib/check_mk_agent/local
