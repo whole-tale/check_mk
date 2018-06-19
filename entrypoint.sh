b0da99310e6baf8bd9ceaaab331201e0e23a58d8
Craig Willis <willis8@illinois.edu>
2018-06-19 14:45:56 -0500

    Change check-tale to run every 10 minutes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ mkdir -p /usr/lib/check_mk_agent/local
 cp /scripts/check-services /usr/lib/check_mk_agent/local
 cp /scripts/check-nodes /usr/lib/check_mk_agent/local
 cp /scripts/check-celery /usr/lib/check_mk_agent/local
+cp /scripts/check-tale /usr/lib/check_mk_agent/local/600/
 
 /usr/sbin/xinetd -f /etc/xinetd.conf -dontfork -stayalive
